f707479cd7de1a918278344d9c811e93c3633a5d
makwetjat <70511068+makwetjat@users.noreply.github.com>
2024-08-07 14:38:39 +0200

    Update yum-update.sh

--- BCX-RHEL-Scripts/yum-update.sh
+++ BCX-RHEL-Scripts/yum-update.sh
@@ -6,15 +6,14 @@ if sudo subscription-manager status | grep -q -e "Content Access Mode is set to 
     # Perform yum update, suppress output but keep errors
     if sudo yum update -y > /dev/null; then
        echo "Yum update completed successfully."
+       echo "Yum update completed. Scheduling reboot."
+       # Schedule the reboot for 1 minute later to allow Jenkins to finish the script
+       sudo shutdown -r
+       echo "Update completed. Server will reboot in 1 minute."
+       echo "Jenkins job should disconnect now."
     else
        echo "Yum update failed. Please check the error messages above."
-       exit 1
     fi
-    echo "Yum update completed. Scheduling reboot."
-    # Schedule the reboot for 1 minute later to allow Jenkins to finish the script
-    sudo shutdown -r
-    echo "Update completed. Server will reboot in 1 minute."
-    echo "Jenkins job should disconnect now."
 else
     echo "Server is not registered with the Satellite."
     exit 1
