d8b08d37884fb1d23d4ccb60669f20b03bcfc56a
makwetjat <70511068+makwetjat@users.noreply.github.com>
2024-09-04 12:08:59 +0200

    Error handling in scripts

--- BCX-RHEL-Scripts/yum-update.sh
+++ BCX-RHEL-Scripts/yum-update.sh
@@ -3,8 +3,8 @@
 # Check if the server is subscribed
 if sudo subscription-manager status | grep -q -e "Content Access Mode is set to Simple Content Access. This host has access to content, regardless of subscription status." -e "Overall Status: Current"; then
     echo "Server is registered. Proceeding with yum update."
-    # Perform yum update, suppress output but keep errors
-    if sudo yum update -y > /dev/null; then
+    # Perform yum update, suppress standard output but keep errors
+    if sudo yum update -y > /dev/null 2>&1; then
        echo "Yum update completed successfully."
        echo "Yum update completed. Scheduling reboot."
        # Schedule the reboot for 1 minute later to allow Jenkins to finish the script
@@ -12,11 +12,13 @@ if sudo subscription-manager status | grep -q -e "Content Access Mode is set to 
        echo "Update completed. Server will reboot in 1 minute."
        echo "Jenkins job should disconnect now."
     else
-       echo "Yum update failed. Please check the error messages above."
+       echo "Yum update failed. Please check the error messages above." >&2  # Send this to stderr to ensure Jenkins catches the error
+       exit 1  # Explicitly exit with an error status if yum update fails
     fi
 else
-    echo "Server is not registered with the Satellite."
+    echo "Server is not registered with the Satellite." >&2  # Send this to stderr
     exit 1
 fi
 
+# Clean up script file
 rm -rf /tmp/yum-update.sh
